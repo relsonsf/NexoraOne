f971df272f285d94b52b964f34b23c4f79e676be
Relson Souza Ferreira <relson.ferreira@gmail.com>
2025-09-08 16:20:39 -0300

    AjusteHeader

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           <div className="flex items-center space-x-2">
-            <img src="/nexoraoneLogo.png" alt="Nexora One" className="w-10 h-10" />
+            <img src={`${import.meta.env.BASE_URL}nexoraoneLogo.png`} alt="Nexora One" className="w-10 h-10"/>
             <span className={`text-2xl font-bold ${isScrolled ? 'text-gray-900' : 'text-white'}`}>
               Nexora One
             </span>
